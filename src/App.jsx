122093664447feb0a6fd492b8b97b27cfd1d8152
thomas <thomaslazzeri3@gmail.com>
2026-07-17 18:39:47 +0200

    add useState article function map

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 
 
-export default App
+export const App = () => {
+    const [articles, setArticles] = useState([]);
+
+    retun(
+        <>
+            <ul>
+                {articles.map(article => (
+                    <li key={article}>{article}</li>
+                ))}
+            </ul>
+        </>
+    )
+};
