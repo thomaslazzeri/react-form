6a4f8fe48480a25dbd67c35c3220734a41218f13
thomas <thomaslazzeri3@gmail.com>
2026-07-18 10:43:49 +0200

    Fix syntax errors and improve formatting in App and main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import { useState } from 'react'
-import './App.css'
-
-
+import { useState } from 'react';
+import './App.css';
 
 export const App = () => {
     const [articles, setArticles] = useState([]);
+    const [inputValue, setInputValue] = useState('');
 
-    retun(
+    return (
         <>
             <ul>
                 {articles.map(article => (
                     <li key={article}>{article}</li>
                 ))}
             </ul>
+            <form onSubmit={e => {
+                e.preventDefault();
+                setArticles(articles => articles.concat(inputValue));
+                setInputValue('');
+            }}>
+                <input type="text" 
+                placeholder='Titolo articolo' 
+                value={inputValue} 
+                onChange={e => setInputValue(e.target.value)}/>
+               
+                <button type="submit">Aggiungi</button>
+            </form>
         </>
-    )
+    );
 };
